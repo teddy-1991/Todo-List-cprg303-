c59ab326be4a41147c679a42b92f2fba63d3cf55
Jooyoung Kim <yongin016@gmail.com>
2024-09-23 12:43:41 -0600

    .

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+
+      <Text style={{ fontSize: 25 }}>I am in React native class</Text>
+  
+    </View>
+  );
+};
+
+export default Index;
+
